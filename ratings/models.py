f957a5874213787625a5099952e4a5f5c24282aa
Connor <conarm64@gmail.com>
2025-02-10 17:10:22 +0000

    Got rid of old models

--- ratings/models.py
+++ ratings/models.py
@@ -1,17 +1,5 @@
 from django.db import models
-
-# Tutorial models
-# class Question(models.Model):
-#     question_text = models.CharField(max_length=200)
-#     pub_date = models.DateTimeField("date published")
-
-
-# class Choice(models.Model):
-#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#     choice_text = models.CharField(max_length=200)
-#     votes = models.IntegerField(default=0)
     
-# My own models
 # TODO: check f2f relation parameters make sense
 # TODO: check if year is stored correctly
 # TODO: check if we need to limit integer - especially year and semester
