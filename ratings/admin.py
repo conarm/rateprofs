ef61b2d895637551c56272d2a618d00a461ec30d
Connor <conarm64@gmail.com>
2025-02-10 17:07:26 +0000

    Set up models and admin

--- ratings/admin.py
+++ ratings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Module, Professor, ModuleInstance
 
-# Register your models here.
+# Register your models here
+
+admin.site.register(User)
+admin.site.register(Module)
+admin.site.register(Professor)
+admin.site.register(ModuleInstance)
--- ratings/models.py
+++ ratings/models.py
@@ -1,12 +1,36 @@
 from django.db import models
 
-# Create your models here.
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+# Tutorial models
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField("date published")
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
+    
+# My own models
+# TODO: check f2f relation parameters make sense
+# TODO: check if year is stored correctly
+# TODO: check if we need to limit integer - especially year and semester
+# TODO: check whether to use "professors" or "teachers" on a module instance
+
+class User(models.Model):
+    username = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    password = models.CharField(max_length=250)
+    
+class Module(models.Model):
+    name = models.CharField(max_length=250)
+    
+class Professor(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.CharField(max_length=250)
+    
+class ModuleInstance(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    semester = models.IntegerField()
+    professors = models.ManyToManyField(Professor, related_name='modules')
